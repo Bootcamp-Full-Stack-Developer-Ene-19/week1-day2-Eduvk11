e5945634e661b116dc600aecc86dee2e8b94d260
Eduardo <eduvk11@hotmail.com>
2019-01-20 23:32:41 +0100

    Home Page.html

--- Home Page.html/js/toggler.js
+++ Home Page.html/js/toggler.js
@@ -2,14 +2,15 @@ function init() {
 
   // Dropdown
   // --------------------------------------------------------
-  var dropdown = document.querySelector('.dropdown__btn');
-
-  dropdown.onclick = function(event) {
-    var dropdown = this;
-    if (dropdown.hasAttribute('aria-expanded') && dropdown.getAttribute('aria-expanded') === 'false') {
-      dropdown.setAttribute('aria-expanded', 'true');
-    } else {
-      dropdown.setAttribute('aria-expanded', 'false');
+  var dropdown = document.querySelectorAll('.dropdown__btn');
+  for (var i = 0; i < dropdown.length; i++){ 
+    dropdown[i].onclick = function(event) {
+      var dropdown = this;
+      if (dropdown.hasAttribute('aria-expanded') && dropdown.getAttribute('aria-expanded') === 'false') {
+        dropdown.setAttribute('aria-expanded', 'true');
+      } else {
+        dropdown.setAttribute('aria-expanded', 'false');
+      }
     }
   }
 
